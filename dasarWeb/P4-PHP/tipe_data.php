f390d61f2664ee644cdfa3ce664f3552b2ec2da9
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-09-17 08:28:47 +0700

    Bagian 2: Penggunaan Tipe Data_Langkah 5

--- dasarWeb/P4-PHP/tipe_data.php
+++ dasarWeb/P4-PHP/tipe_data.php
@@ -12,4 +12,17 @@ echo "Variabel d: {$d} <br>";
 echo "Variabel d: {$e} <br>";
 
 var_dump($e);
+
+$nilaiMatematika = 5.1;
+$nilaiIPA = 6.7;
+$nilaiBahasaINdonesia = 9.3;
+
+$rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaINdonesia) / 3;
+
+echo "Matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaINdonesia} <br>";
+echo "Rata-rata: {$rataRata} <br>";
+
+var_dump($rataRata);
 ?>
