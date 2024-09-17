8ff550e3fb7fb9b16e74b39308b9d5f4949cd30f
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-09-17 09:16:50 +0700

    Bagian 3: Penggunaan Operator PHP_Soal Cerita

---
+++ dasarWeb/P4-PHP/tugas_operator_bagian3.php
@@ -0,0 +1,10 @@
+<?php 
+
+$kursi = 45;
+$pelanggan = 28;
+$prsKursiKosong = ((45-28) / 45) * 100;
+
+echo "Sebuah restoran memiliki 45 kursi di dalamnya. Pada suatu malam, 28 kursi telah ditempati oleh pelanggan. 
+        Berapa persen kursi yang masih kosong di restoran tersebut?<br><br>";
+echo "presentase kursi yang masih kosong adalah {$prsKursiKosong}%.";
+?>
