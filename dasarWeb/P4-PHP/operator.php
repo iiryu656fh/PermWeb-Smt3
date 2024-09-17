d81c294548f37702c98aab98d53690bd0c512b70
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-09-17 08:49:23 +0700

    Bagian 3: Penggunaan Operator PHP_Langkah 3

--- dasarWeb/P4-PHP/operator.php
+++ dasarWeb/P4-PHP/operator.php
@@ -8,4 +8,13 @@ $hasilKali = $a * $b;
 $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat = $a ** $b;
+
+echo "a = 10, b = 5 <br>";
+echo "<br>a + b     : {$hasilTambah}<br>";
+echo "a - b     : {$hasilKurang}<br>";
+echo "a * b     : {$hasilKali}<br>";
+echo "a / b     : {$hasilBagi}<br>";
+echo "a % b     : {$sisaBagi}<br>";
+echo "a ^ b     : {$pangkat}<br>";
+
 ?>
