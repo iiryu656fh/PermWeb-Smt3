3965b92938e428a7b79213525aff183e94d9a9f9
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-09-17 09:48:40 +0700

    Bagian 4: Penggunaan Struktur Kontrol pada PHP_Langkah 14

--- dasarWeb/P4-PHP/struktur_kontrol.php
+++ dasarWeb/P4-PHP/struktur_kontrol.php
@@ -52,4 +52,12 @@ for ($i = 1; $i <= $jumlahLahan; $i++){
 echo "<br>";
 echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
 
+echo "<br><br><br>";
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor += $skor;
+}
+echo "Total skor ujian adalah: $totalSkor";
 ?>
