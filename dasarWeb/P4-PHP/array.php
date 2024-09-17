2238faa184a770189afc389452176cb0b98fade7
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-09-17 12:46:01 +0700

    Bagian 5: Penggunaan Array pada PHP_Langkah 6

--- dasarWeb/P4-PHP/array.php
+++ dasarWeb/P4-PHP/array.php
@@ -10,4 +10,23 @@ foreach ($nilaiSiswa as $nilai) {
 }
 
 echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus);
+echo "<br><br><br>";
+
+$daftarKaryawan = [
+    ['Alice', 7],
+    ['Bob', 3],
+    ['Charlie', 9],
+    ['David', 5],
+    ['Eva', 6],
+];
+
+$karyawanPengalamanLimaTahun =[];
+
+foreach ($daftarKaryawan as $Karyawan) {
+    if ($karyawan[1] > 5) {
+        $karyawanPengalamanLimaTahun[] = $karyawan[0];
+    }
+}
+
+echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: " . implode(', ', $karyawanPengalamanLimaTahun);
 ?>
