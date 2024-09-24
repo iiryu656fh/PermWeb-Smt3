000afa7c0c5f8ef3f8736bfd13091270044079f3
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-09-24 08:06:31 +0700

    Fungsi Rekursif - Menampilkan Angka - Langkah 4

--- dasarWeb/P5-PHP2/rekursif.php
+++ dasarWeb/P5-PHP2/rekursif.php
@@ -8,8 +8,23 @@
 // tampilkanHaloDunia();
 
 
-//Langkah 3 - Menampilkan Angka
+//Langkah 3 - Menampilkan Angka dengan perulangan for
+echo "Perulangan For";
 for ($i = 1; $i <= 25; $i++){
     echo "Perulangan ke-{$i} <br>";
 }
+echo "<hr>";
+echo "Fungsi Rekursif<br/>";
+// Lengkah 4 - Menampilkan Angka dengan Fungsi Rekursif
+function tampilkanAngka(int $jumlah, int $indeks = 1){
+    echo "Perulangan ke-{$indeks} <br>";
+
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah ){
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+tampilkanAngka(20);
+
+
 ?>
