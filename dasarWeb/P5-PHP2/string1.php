c4067d67b3c0fa6e8d6b2698d36f2cbbb9d092f7
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-09-24 08:49:05 +0700

    String - Escape Character -  langkah 3

--- dasarWeb/P5-PHP2/string1.php
+++ dasarWeb/P5-PHP2/string1.php
@@ -12,16 +12,4 @@ echo "<p>" . strtoupper($loremIpsum) . "</p>";
 echo "<p>" . strtolower($loremIpsum) . "</p>";
 
 echo "<hr>";
-//Escape Caracter
-echo "Baris\nbaru <br>"; //soal 10.a
-echo 'Baris\nbaru <br>'; //soal 10.b
-echo "Halo\rDunia <br>"; //soal 10.c
-echo 'Halo\rDunia <br>'; //soal 10.d
-
-echo "<pre>Halo\tDunia!</pre>"; //spal 10.e
-echo '<pre>Halo\tDunia!</pre>'; //spal 10.f
-
-echo "Katakanlah \"Tidak pada narkoba!\" <br>"; //soal 10.g
-echo 'Katakanlah \'Tidak pada narkoba!\' <br>'; //soal 10.g
-
 ?>
