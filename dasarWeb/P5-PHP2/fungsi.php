aca48be9b66ed130d0a45556195619ce8f04948d
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-09-24 07:34:22 +0700

    Fungsi dengan parameter (Langkah 4)

--- dasarWeb/P5-PHP2/fungsi.php
+++ dasarWeb/P5-PHP2/fungsi.php
@@ -1,12 +1,29 @@
 <?php 
 
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Nisa <br/>"; //Tulis sesuai nama kalian
-    echo "Senang berkenalan dengan Anda <br/>";
+// tabel 1
+// function perkenalan(){
+//     echo "Assalamualaikum, ";
+//     echo "Perkenalkan, nama saya Nisa <br/>"; //Tulis sesuai nama kalian
+//     echo "Senang berkenalan dengan Anda <br/>";
+// }
+
+// //Memanggil fungsi yang sudah dibuat
+// perkenalan();
+// perkenalan();
+
+//tabel 2 - membuat fungsi dengan parameter
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
 }
 
-//Memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+perkenalan("Hamdana", "Hallo");
+
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan($saya, $ucapanSalam);
 ?>
