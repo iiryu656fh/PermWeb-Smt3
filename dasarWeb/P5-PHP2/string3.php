d2f459b2bac248bcb90bb14d22a858234145a635
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-09-24 09:02:50 +0700

    String - Membalik String per kata

--- dasarWeb/P5-PHP2/string3.php
+++ dasarWeb/P5-PHP2/string3.php
@@ -1,5 +1,18 @@
 <?php 
-
+//langkah 5 - membalikkan string
 $pesan = "Saya arek malang";
 echo strrev($pesan) .  "<br>";
+
+echo "<hr>";
+
+//langkah 8 - membalikkan string per kata
+$pesan2 = "saya arek malang";
+# ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerKata = explode(" ", $pesan);
+# ubah Setiap kata dalam array menjadi kebalikannya
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+# gabungkan kembali array menjadi string
+$pesan = implode(" ", $pesanPerKata);
+
+echo $pesan . "<br>";
 ?>
