7caeddcdeb9cb1ad5c452c008491cb0a3456d4fe
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-10-01 08:13:36 +0700

    Praktikum 2: Associative Array - Menambah tabel

--- dasarWeb/P6-PHP-Array/array_2.php
+++ dasarWeb/P6-PHP-Array/array_2.php
@@ -4,6 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title></title>
+        <style>
+            table, td, th {
+                border: 1px solid;
+            }
+
+            table {
+                width: 100%;
+                border-collapse: collapse;
+            }
+        </style>
     </head>
     <body>
         <?php 
@@ -16,6 +26,19 @@
             echo "Nama : {$Dosen ['nama']} <br>";
             echo "Domisili : {$Dosen ['domisili']} <br>";
             echo "Jenis Kelamin : {$Dosen ['jenis_kelamin']} <br>";
+
         ?>
+        <table>
+            <tr>
+                <th>Nama</th>
+                <th>Domisili</th>
+                <th>Jenis Kelamin</th>
+            </tr>
+            <tr>
+                <td> <?php echo $Dosen ['nama']?></td>
+                <td> <?php echo  $Dosen ['domisili']?></td>
+                <td> <?php echo $Dosen ['jenis_kelamin']?></td>
+            </tr>
+        </table>
     </body>
 </html>
