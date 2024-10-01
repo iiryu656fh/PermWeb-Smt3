4dd80f214f799618028a6f55e6b75c80c2ff26fa
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-10-01 08:33:46 +0700

    Praktikum 4: Fungsi dengan parameter

--- dasarWeb/P6-PHP-Array/fungsi.php
+++ dasarWeb/P6-PHP-Array/fungsi.php
@@ -1,11 +1,30 @@
 <?php 
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Nisa <br/>"; //Tulis sesuai nama kalian
-    echo "Senang berkenalan dengan Anda <br/>";
+// function perkenalan(){
+//     echo "Assalamualaikum, ";
+//     echo "Perkenalkan, nama saya Nisa <br/>"; //Tulis sesuai nama kalian
+//     echo "Senang berkenalan dengan Anda <br/>";
+// }
+
+// //Memanggil fungsi yang sudah dibuat
+// perkenalan();
+// perkenalan();
+
+
+
+//tabel 2 - membuat fungsi dengan parameter
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
 }
 
-//Memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+perkenalan("Hamdana", "Hallo");
+
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan($saya, $ucapanSalam);
+
 ?>
