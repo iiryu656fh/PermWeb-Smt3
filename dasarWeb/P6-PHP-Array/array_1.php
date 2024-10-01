eb032b144d1e802b92959c0e37751f89d2922fa0
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-10-01 07:21:10 +0700

    Praktikum 1: Indexed Array - Perulangan for langkah 4

--- dasarWeb/P6-PHP-Array/array_1.php
+++ dasarWeb/P6-PHP-Array/array_1.php
@@ -7,9 +7,12 @@
         <h2>Array Terindeks</h2>
         <?php 
             $Listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
-            echo $Listdosen[2] . "<br>";
-            echo $Listdosen[0] . "<br>";
-            echo $Listdosen[1] . "<br>";
+            for ($i=0; $i <count($Listdosen) ; $i++) { 
+                echo $Listdosen[$i] . "<br>";
+            }
+            // echo $Listdosen[2] . "<br>";
+            // echo $Listdosen[0] . "<br>";
+            // echo $Listdosen[1] . "<br>";
         ?>
     </body>
 </html>
