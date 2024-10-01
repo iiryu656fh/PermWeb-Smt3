669b6b5d99abfc93fc99c9616c131d1826979f42
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-10-01 08:36:04 +0700

    Praktikum 4: Fungsi dengan parameter nilai default

--- dasarWeb/P6-PHP-Array/fungsi.php
+++ dasarWeb/P6-PHP-Array/fungsi.php
@@ -12,19 +12,41 @@
 
 
 //tabel 2 - membuat fungsi dengan parameter
-function perkenalan($nama, $salam){
+// function perkenalan($nama, $salam){
+//     echo $salam.", ";
+//     echo "Perkenalkan, nama saya ".$nama."<br/>";
+//     echo "Senang berkenalan dengan anda<br/>";
+// }
+
+// perkenalan("Hamdana", "Hallo");
+
+// echo "<hr>";
+
+// $saya = "Elok";
+// $ucapanSalam = "Selamat pagi";
+
+// perkenalan($saya, $ucapanSalam);
+
+
+
+// Tabel 3 - Parameter dengan Nilai Default
+function perkenalan ($nama, $salam="Assalamualaikum"){
     echo $salam.", ";
     echo "Perkenalkan, nama saya ".$nama."<br/>";
-    echo "Senang berkenalan dengan anda<br/>";
 }
 
-perkenalan("Hamdana", "Hallo");
+//memanggil fungsi yang sudah dibuat
+perkenalan("Nisa","Halo");
 
 echo "<hr>";
 
-$saya = "Elok";
+$saya = "Iir";
 $ucapanSalam = "Selamat pagi";
 
-perkenalan($saya, $ucapanSalam);
+//Memanggil lagi tanpa mengisi parameter salam
+perkenalan($saya);
+
+
+
 
 ?>
