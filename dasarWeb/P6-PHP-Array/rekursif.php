0d6d4d120ada9eacae80a3b416f893983ce4e119
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-10-01 08:45:29 +0700

    Praktikum 5: Fungsi Rekursif

--- dasarWeb/P6-PHP-Array/rekursif.php
+++ dasarWeb/P6-PHP-Array/rekursif.php
@@ -1,10 +1,30 @@
 <?php 
-function tampilkanHaloDunia(){
-    echo "Halo Dunia! <br>";
+// function tampilkanHaloDunia(){
+//     echo "Halo Dunia! <br>";
 
-    tampilkanHaloDunia();
+//     tampilkanHaloDunia();
+// }
+
+// tampilkanHaloDunia();
+
+
+
+//Langkah 3 - Menampilkan Angka dengan perulangan for
+echo "Perulangan For";
+for ($i = 1; $i <= 25; $i++){
+    echo "Perulangan ke-{$i} <br>";
 }
+echo "<hr>";
 
-tampilkanHaloDunia();
 
-?>
+
+// Lengkah 4 - Menampilkan Angka dengan Fungsi Rekursif
+function tampilkanAngka(int $jumlah, int $indeks = 1){
+    echo "Perulangan ke-{$indeks} <br>";
+
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah ){
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+tampilkanAngka(20);
