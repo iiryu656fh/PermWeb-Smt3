c07840a907816185da9b562ee54aab61a9e18f0d
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-10-08 07:23:19 +0700

    Praktikum 1: Function Isset - langkah 5

--- dasarWeb/P7-PHP-Form/isset.php
+++ dasarWeb/P7-PHP-Form/isset.php
@@ -5,4 +5,13 @@
     } else {
         echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
     }
+
+    echo "<hr>";
+    
+    $data = array("nama" => "Jane", "usia" => 25);
+    if (isset($data["nama"])) {
+        echo "Nama: " . $data["nama"];
+    } else {
+        echo "Variabel 'nama' tidak ditemukan dalam array";
+    }
 ?>
