d4fb521284b541ff44b1b7ca89db16f51cf91b3b
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-10-08 16:24:31 +0700

    Praktikum 4: HTML Injection - langkah 7

--- dasarWeb/P7-PHP-Form/html_aman.php
+++ dasarWeb/P7-PHP-Form/html_aman.php
@@ -8,31 +8,51 @@
         <?php 
             $inputErr = "";
             $input = "";
+            $emailErr = "";
+            $email = "";
+            
             if ($_SERVER["REQUEST_METHOD"] == "POST"){
                 if (empty($_POST["input"])){
                     $inputErr = "Inputan tidak boleh kosong!";
                 } else {
                     $input = $_POST['input'];
                     $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-                    echo "Data berhasil disimpan!";
                 }
+
+                // Memeriksa apakah input adalah email yang valid
+                if (empty($_POST["email"])){
+                    $emailErr = "Masukkan email yang valid!";
+                } else {
+                    $email = $_POST['email'];   
+                    $email = htmlspecialchars($email, ENT_QUOTES, 'UTF-8');                     
+                    if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+                     // lenjutkan dengan pengolahan email yang aman
+                        
+                    } else {
+                        // Tangani input yang tidak valid
+                        $emailErr = "Masukkan email yang valid!";
+                    }
+                    
+                    if (empty($inputErr) && empty($emailErr)) {
+                        echo "Data berhasil disimpan!<br>";
+                        echo "Input: " . $input . "<br>";
+                        echo "Email: " . $email;
+                    }
+                }  
             } 
 
-            // Memeriksa apakah input adalah email yang valid
-            $email = $_POST['email'];
-            if (filter_var($email, FILTER_VALIDATE_EMAIL)){
-                // lenjutkan dengan pengolahan email yang aman
-            } else {
-                // Tangani input yang tidak valid
-            }
         ?>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
             <label for="input">Input:</label>
             <input type="text" name="input" id="input" value="<?php echo $input; ?>">
             <span class="error"><?php echo $inputErr; ?></span><br><br>
             
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" value="<?php echo $email; ?>">
+            <span class="error"><?php echo $emailErr; ?></span><br><br>
             <input type="submit" name="submit" value="Submit">
         </form>
+        
         
     </body>
 </html>
