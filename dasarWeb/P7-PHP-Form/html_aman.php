b6d439424194b2cf710dada1c3a709e6911408f2
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-10-08 08:39:58 +0700

    Praktikum 4: HTML Injection - langkah 3

--- dasarWeb/P7-PHP-Form/html_aman.php
+++ dasarWeb/P7-PHP-Form/html_aman.php
@@ -1,4 +1,30 @@
-<?php 
-    $input = $_POST['input'];
-    $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HTML Injection</title>        
+    </head>
+    <body>
+        <h2>Form Input PHP Injection</h2>
+        <?php 
+            $inputErr = "";
+            $input = "";
+            if ($_SERVER["REQUEST_METHOD"] == "POST"){
+                if (empty($_POST["input"])){
+                    $inputErr = "Inputan tidak boleh kosong!";
+                } else {
+                    $input = $_POST['input'];
+                    $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+                    echo "Data berhasil disimpan!";
+                }
+            } 
+        ?>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <label for="input">Input:</label>
+            <input type="text" name="input" id="input" value="<?php echo $input; ?>">
+            <span class="error"><?php echo $inputErr; ?></span><br><br>
+            
+            <input type="submit" name="submit" value="Submit">
+        </form>
+        
+    </body>
+</html>
