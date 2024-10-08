56e9741598d74fcd4eb4126a0d42161fec86c919
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-10-08 16:36:44 +0700

    Praktikum 5: penggunaan Regex pada PHP - langkah  14 preg_match

--- dasarWeb/P7-PHP-Form/regex.php
+++ dasarWeb/P7-PHP-Form/regex.php
@@ -21,6 +21,15 @@
     $replacement = 'banana';
     $text = 'I Like apple pie.';
     $new_text = preg_replace($pattern, $replacement, $text);
-    echo $new_text; // 
+    echo $new_text; 
+
+    echo "<hr>";
+    $pattern = '/go*d/'; // cocokkan "god", "good", "gooood", dll
+    $text = 'god is good.';
+    if (preg_match($pattern, $text, $matches)){
+        echo "Cocokkan: " . $matches[0];
+    } else {
+        echo "Tidak ada yang cocok!";
+    }
 
 ?>
