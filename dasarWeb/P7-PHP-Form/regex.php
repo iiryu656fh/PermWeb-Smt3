c6752742c09dfee655baa9c9ebf91c8a098e53e4
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-10-08 16:31:30 +0700

    Praktikum 5: penggunaan Regex pada PHP - langkah 6

--- dasarWeb/P7-PHP-Form/regex.php
+++ dasarWeb/P7-PHP-Form/regex.php
@@ -6,4 +6,14 @@
     } else {
         echo "Tidak ada huruf kecil!";
     }
+
+    echo "<hr>";
+    $pattern = '/[0-9]+/'; // cocokkan satu datau lebih digit
+    $text = 'There are 123 apples';
+    if (preg_match($pattern, $text, $matches)){
+        echo "Cocokkan: " . $matches[0];
+    } else {
+        echo "Tidak ada yang cocok!";
+    }
+
 ?>
