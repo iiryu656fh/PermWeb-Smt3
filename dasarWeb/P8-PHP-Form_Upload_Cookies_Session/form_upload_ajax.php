e355e6f659d55480397a71cb041253bea22f18ef
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-10-18 21:46:27 +0700

    Praktikum 3: Upload File dengan PHP dan JQuery - langkah 6

--- dasarWeb/P8-PHP-Form_Upload_Cookies_Session/form_upload_ajax.php
+++ dasarWeb/P8-PHP-Form_Upload_Cookies_Session/form_upload_ajax.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Unggah File Dokumen</title>
+        <title>Unggah File Gambar</title>
     </head>
 
     <body>
         <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="file" id="file">
+            <input type="file" name="files[]" id="file" multiple>
             <input type="submit" name="submit" value="Unggah">
         </form>
         <div id="status"></div>
