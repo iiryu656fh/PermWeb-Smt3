6f5651fed83d1e9d47e86f52d97c09daff8651d1
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-11-10 21:12:30 +0700

    commit

--- crud_hijab/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$serverName = "LAPTOP-O7MSCG4B\SQLEXPRESS";
-$connectionInfo = [
-    "Database" => "crudhijab"
-];
-
-$conn = sqlsrv_connect($serverName, $connectionInfo) or die(print_r(sqlsrv_errors(), true));
